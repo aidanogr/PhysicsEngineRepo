cc28dc9529a67257676efdc97afe5e7547cdde1a
Aidan O'Grady <aidanhenryrc@gmail.com>
2025-04-07 15:00:44 -0500

    buffer commit
    about to change compiler stuff, commiting now so i can revert if everything breaks

---
+++ Animation_Library_V0_1_0/Java_CLI_Handler.c
@@ -0,0 +1,40 @@
+#include "animator.h"
+#include <stdint.h>
+
+
+//The function serves as the tunnel between the java CLI and C code to be called by it, 
+//all data is sanatized before use of entry
+int entry(char* input_file_path, char* output_file_path, int index_of_focused_mass,
+	double bounds[], int8_t keep_intermediate_files) {
+    
+    int err = open_ppm_sim(input_file_path);
+    if(err != 0) {
+	printf("error opening sim file: code %d\n", err);
+	return -1;
+    }
+
+
+    if(index_of_focused_mass < 0) {
+	//xmin,ymin,xmax,ymax
+	set_bounds(bounds[0], bounds[1], bounds[2], bounds[3]);
+	err = initialize_animation(-1);
+	if(err != 0) {
+	    printf("error initializing animation, code %d", err);
+	    return -1;
+	}
+    }
+    else {
+	err = initialize_animation(0);
+	if(err != 0) {
+	    printf("error initializing animation: code %d\n", err);
+	    return -1;
+	}
+    }
+
+    err = animate();
+    if(err != 0) {
+	printf("\nunexpected error animating simulation: code: %d", err); 
+	return -1;
+    }
+    return 0;
+}
