1c363ea1388e8e309192ec682f506cc5f330ea4c
Aidan O'Grady <aidanhenryrc@gmail.com>
2025-04-08 14:47:25 -0500

    Remove build files from github

--- Animation_Library_V0_1_0/build_files/CMakeFiles/Java_CLI.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Java_CLI.
-# This may be replaced when dependencies are built.
--- Physics_Engine_v0_0_1/build_files/CMakeFiles/Engine.dir/src/Utility.c.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Engine.dir/src/Utility.c.o: \
-  /Users/aidanogrady/Documents/GitHub/PhysicsEngineRepo/Physics_Engine_v0_0_1/src/Utility.c
